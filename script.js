277b9c4d04748ece1623258350671a73a56e493c
Kc2693 <kailin.cannon@gmail.com>
2020-07-17 15:11:29 -0600

    alphabetical sort setup

--- script.js
+++ script.js
@@ -14,42 +14,53 @@ $('.show-specific-select').change(function() {
   if ($('.show-specific-select').val() == 'All') {
     $(".quantity").val('')
     $('.order-total').text('0')
-    // $('.item').css('display', 'inline-block');
     $('.item').toggleClass('show-specific-select-hide-toggle', false)
 
   } else {
     let rawVal = $('.show-specific-select').val().split(" ")
     $(".quantity").val('')
     $('.order-total').text('0')
-    // $('.item').css('display', 'none');
     $('.item').toggleClass('show-specific-select-hide-toggle', true)
 
     rawVal.forEach(word => {
-      // $(`span:contains("${word}")`).parent().css('display', 'inline-block');
       $(`span:contains("${word}")`).parent().toggleClass('show-specific-select-hide-toggle', false)
     });
   }
 });
+
+$('.sort-select').change(function(){
+  let parentColumn;
+  $('.col-md-3').each(function(index, elem) {
+    parentColumn = "#" + elem.getAttribute('id')
+    itemSelector = parentColumn + " > .item"
+
+    $(itemSelector).sort(function(a,b) {
+
+      if (a.textContent < b.textContent) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }).appendTo(parentColumn)
+  })
+});
   
 function gatherItemTotals() {
-
   let calculated = $('.quantity').map((i, item) => {
-  
-	let quantity = $(item).val();
-  let itemPrice = parseInt($(item).siblings(".item-price").text());
-    
-  return quantity * itemPrice;
+	  let quantity = $(item).val();
+    let itemPrice = parseInt($(item).siblings(".item-price").text()); 
+
+    return quantity * itemPrice;
+  })
    
-	}) 
 	return calculated.get();
 }
 
 function reduceOrderTotal(itemTotals) {  
 	let orderTotal = itemTotals.reduce((currentTotal, item) => {
-  	let whatthefuck = currentTotal + item;
+  	let newTotal = currentTotal + item;
 
-    return whatthefuck
+    return newTotal;
   });
-  console.log(orderTotal)
   return orderTotal;
 }
